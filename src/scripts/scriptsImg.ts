682186354726aea0edee63c71756f03e8fb2f916
Kleyton Ferreira <kleytondev40@gmail.com>
2026-05-07 15:54:50 -0300

    feat: I added images and a script with the images.

---
+++ src/scripts/scriptsImg.ts
@@ -0,0 +1,9 @@
+import Bg from "../../public/imgs/bg.png"
+import Logo from "../../public/imgs/logo.png"
+import Modelo from "../../public/imgs/image-modelo.png"
+
+export const Images = {
+    bg: Bg,
+    logo: Logo,
+    modelo: Modelo
+}
