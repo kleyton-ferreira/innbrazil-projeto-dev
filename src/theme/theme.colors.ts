0e01dbe314c90db8148b1b2e1e214d3e724bacd1
Kleyton Ferreira <kleytondev40@gmail.com>
2026-05-07 16:07:11 -0300

    feat: add themes colors styles

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,30 @@
+const Colors = {
+    text: {
+        white: "#ffffff",
+        gray: "#4a555c",
+        yellow: "#ffcc00",
+        pink: "#e6007e",
+        pinkLight: "#e47cb0",
+        buttonwaths: "#000000",
+    },
+    background: {
+        backgroundCardsYellow: "#fac208",
+        backgroundPink: "#e50b81",
+        backgroundWats: "#36ff5b",
+        backgroundBorderWats: "#78de87",
+        backgroundIconWats: "#9effb0",
+        backgroundWatsIcon: "#05931a",
+    },
+    backgroundCards: {
+        backgroundCardsOrangetop: "#fb6609",
+        backgroundCardsOrangebody: "#fb9e09",
+        backgroundCardsCellowTop: "#fb8a09",
+        backgroundCardsYellowBody: "#fac208",
+        backgroundCardsBlackTop: "#4a555c",
+        backgroundCardsBlackBody: "#000000",
+        backgroundCardsBlueTop: "#69b1cb",
+        backgroundCardsBlueBody: "#3e4dba"
+    }
+}
+
+export default Colors
