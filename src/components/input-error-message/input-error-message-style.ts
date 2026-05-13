2c21ed4f2721251e7418a1899de6722cd1cf09d3
Kleyton Ferreira <kleytondev40@gmail.com>
2026-05-13 13:05:25 -0300

    feat: add input error components

---
+++ src/components/input-error-message/input-error-message-style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Colors from '../../theme/theme.colors'
+
+export const InputErrorMessageContainer = styled.div`
+  width: 100%;
+  color: ${Colors.input.error};
+  font-size: 17px;
+  margin-top: 2px;
+  font-weight: 400;
+`
