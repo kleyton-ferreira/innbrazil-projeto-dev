475b4fba9f287bb6718b58ea287af8d5e7927272
Kleyton Ferreira <kleytondev40@gmail.com>
2026-05-13 13:03:41 -0300

    fix: Correcting the section tag to separate the components home

--- src/page/home/home-components.tsx
+++ src/page/home/home-components.tsx
@@ -1,10 +1,18 @@
 // COMPONENTS
 import Header from '../../components/header/header-components'
+import Banner from '../../components/banner/banner-components'
+import Points from '../../components/points/points-components'
+import Cards from '../../components/cards/cards-components'
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <section>
+        <Banner />
+      </section>
+      <Points />
+      <Cards />
     </>
   )
 }
